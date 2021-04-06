1a76725c9258a44b75b12cb2622d0a6384c9f383
amchacko@udel.edu <amchacko@udel.edu>
2021-04-06 10:12:20 -0400

    right button clicked

--- Scene2.js
+++ Scene2.js
@@ -6,31 +6,37 @@ class Scene2 extends Phaser.Scene {
     create() {
         this.background = this.add.image(0, 0, "background");
         this.background.setOrigin(0, 0);
-        this.shovel = this.add.image(config.width / 2, config.height / 2, "shovel");
+       
         this.add.text(20, 20, "Playing Game", { font: "25px Arial", fill: "white" });
 
         this.input.mouse.disableContextMenu();
 
         this.input.on('pointerdown', function (pointer) {
 
-            if (pointer.rightButtonDown())
-            {
+            if (pointer.rightButtonDown()) {
 
-                    this.add.image(pointer.x, pointer.y, 'shovel');
+                this.add.image(pointer.x, pointer.y, 'hose');
+                this.add.text(20, 20, "Right Button Clicked", { font: "25px Arial", fill: "black" });
                 
             }
-            else
-            {
+            else {
    
-                    this.add.image(pointer.x, pointer.y, 'hose');
+                this.add.image(pointer.x, pointer.y, 'shovel');
+                
+    
              
             }
     
         }, this);
+
+    
     }
 
+ 
+
     update ()
-{
+    {
+        // this.moveShovel(this.shovel, 2);
     var pointer = this.input.activePointer;
 
 }
